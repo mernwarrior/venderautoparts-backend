d4d25b47a879f3c36cf713fcd35448165a02574d
mernwarrior <mernwarrior@gmail.com>
2026-05-26 22:06:47 +0530

    testings

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,12 +1,11 @@
 import { createClient } from "redis";
-import { REDIS_HOST, REDIS_PORT } from "./const.js";
 
 const redisClient = createClient({
-  socket: {
-    host: REDIS_HOST || "127.0.0.1",
-    port: REDIS_PORT || 6379,
-  },
-  password: process.env.REDIS_PASSWORD || undefined,
+  url: process.env.REDIS_URL,
+});
+
+redisClient.on("connect", () => {
+  console.log("✅ Redis Connected");
 });
 
 redisClient.on("error", (err) => {
@@ -15,7 +14,6 @@ redisClient.on("error", (err) => {
 
 export const connectRedis = async () => {
   await redisClient.connect();
-  console.log("✅ Redis Connected");
 };
 
 export default redisClient;
